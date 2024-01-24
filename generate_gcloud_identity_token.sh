efe526b279067694b916961c5b547191beed600c
Dawid Dahl <dawid.dahl@umain.com>
2024-01-24 17:23:00 +0100

    update script

--- generate_gcloud_identity_token.sh
+++ generate_gcloud_identity_token.sh
@@ -1,51 +1,48 @@
 #!/bin/bash
 
-# Source the .env.gcloud file
+# Load environment variables from .env.gcloud file
 set -a
-source .env.gcloud
+[ -r ".env.gcloud" ] && source .env.gcloud || { echo ".env.gcloud file not found" >&2; exit 1; }
 set +a
 
-# Initialize a flag to indicate success
+# Initialize a flag to indicate script success
 script_success=false
 
-# Function for cleanup only
+# Function to clean up resources on error
 cleanup() {
     echo "An error occurred. Cleaning up without token generation." >&2
     rm -f "$TEMP_SA_FILE"
 }
 
-# Function for cleanup and returning the token
+# Function to clean up and return the token on success
 cleanup_and_return_token() {
     rm -f "$TEMP_SA_FILE"
     echo "$token"
 }
 
-# Trap to execute cleanup function on script exit
+# Default trap for cleanup
 trap cleanup EXIT
 
-# Check if the SERVICE_ACCOUNT_JSON environment variable is set and not empty
+# Check and validate necessary environment variables
 if [ -z "$SERVICE_ACCOUNT_JSON" ]; then
-    echo "Error: The environment variable 'SERVICE_ACCOUNT_JSON' is undefined or empty." >&2
+    echo "Error: SERVICE_ACCOUNT_JSON is undefined or empty." >&2
     exit 1
 fi
 
-# Check if the GCLOUD_IDENTITY_TOKEN_AUDIENCE environment variable is set and not empty
 if [ -z "$GCLOUD_IDENTITY_TOKEN_AUDIENCE" ]; then
-    echo "Error: The environment variable 'GCLOUD_IDENTITY_TOKEN_AUDIENCE' is undefined or empty." >&2
+    echo "Error: GCLOUD_IDENTITY_TOKEN_AUDIENCE is undefined or empty." >&2
     exit 1
 fi
 
-# Create a temporary file to hold the service account JSON
+# Create a temporary file for the service account JSON
 TEMP_SA_FILE=$(mktemp)
 echo "${SERVICE_ACCOUNT_JSON}" > "${TEMP_SA_FILE}"
 
 # Activate the service account
 gcloud auth activate-service-account --key-file="$TEMP_SA_FILE"
 
-# Try to capture the token
+# Capture the token and check for success
 token=$(gcloud auth print-identity-token --audiences="$GCLOUD_IDENTITY_TOKEN_AUDIENCE" 2> /dev/null)
-
-# Check if token capture was successful
 if [ -n "$token" ]; then
     script_success=true
     trap cleanup_and_return_token EXIT
